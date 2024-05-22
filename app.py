ece69c93c1e35d22c77563c4a2ce01b6e14a193f
李柏霖 <161836618+a31263556@users.noreply.github.com>
2024-05-22 19:52:27 +0800

    Initial commit

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ DATABASE = 'mydb.db'
 logging.basicConfig(filename='error.log', level=logging.ERROR)
 
 def get_db():
+
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
